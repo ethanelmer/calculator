fb52cf324f9b3b199587c44771a3d1a8fd4b72cb
Ethan Elmer <ethankelmer@gmail.com>
2024-02-16 19:34:27 -0600

    Update main.exe

---
+++ build/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/main.cpp.obj"
+  "CMakeFiles/calculator.dir/main.cpp.obj.d"
+  "calculator.exe"
+  "calculator.exe.manifest"
+  "calculator.pdb"
+  "libcalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
